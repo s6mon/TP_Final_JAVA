8f5b42634e42a237ea2e952347c588a53caddff7
Gr05 <lucas.guerry@gmail.com>
2016-04-13 15:16:35 +0200

    ajout attribut comp à

--- src/Circuit/$Port.java
+++ src/Circuit/$Port.java
@@ -3,6 +3,11 @@ package Circuit;
 public abstract class $Port {
 	protected Etat etat;
 	protected boolean connected;
+	protected $Composant comp;
+	
+	public $Composant comp(){
+		return comp;
+	}
 	
 	public Etat getEtat(){return etat;}
 	
